71280080f9c0e1e73fb4d137ddb93e19e73454a5
softprops <d.tangren@gmail.com>
2013-08-22 01:15:34 -0400

    more unit tests

--- src/test/scala/base64/Base64Spec.scala
+++ src/test/scala/base64/Base64Spec.scala
@@ -6,6 +6,7 @@ import org.scalatest.FunSpec
 class Base64Spec extends FunSpec {
 
   describe ("Encode") {
+
     it ("should allow url unsafe output") {
       assert(str(Encode("hello world?")) === "aGVsbG8gd29ybGQ/")
     }
@@ -15,24 +16,35 @@ class Base64Spec extends FunSpec {
         case (in, out) => assert(str(Encode(in)) === out)
       }
     }
+
   }
 
   describe ("Encode.urlSafe") {
+
     it ("should escape url unsafe output") {
       assert(str(Encode.urlSafe("hello world?")) === "aGVsbG8gd29ybGQ_")
     }
+
   }
 
   describe ("Decode") {
+
     it ("should not bother decoding pad only input") {
       ("====" :: "===" :: "==" :: "=" :: Nil).foreach {
         p => assert(Decode(p).right.map(_.size) === Right(0))
       }
     }
+
+    it ("should decode url unsafe strs") {
+      assert(Decode("aGVsbG8gd29ybGQ/").right.map(str) === Right("hello world?"))
+    }
+
   }
 
   describe ("Decode.urlSafe") {
-
+    it ("should decode urlsafe strs") {
+      assert(Decode.urlSafe("aGVsbG8gd29ybGQ_").right.map(str) === Right("hello world?"))
+    }
   }
 
   def str(bytes: Array[Byte]) = new String(bytes)
