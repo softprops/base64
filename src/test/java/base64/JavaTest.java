0a0661f0501fc6b16649b639f628f54bfe6cf69b
softprops <d.tangren@gmail.com>
2014-09-28 18:00:55 -0400

    java usablity test

---
+++ src/test/java/base64/JavaTest.java
@@ -0,0 +1,22 @@
+package base64;
+
+import scala.util.Either;
+
+public class JavaTest {
+  public static void main(String[] args) {
+      // type class instanes
+    Input<String> strs = Input$.MODULE$.Utf8Str();
+    Input<byte[]> bytes = Input$.MODULE$.Bytes();
+    // to and fro
+    Either<Decode.Failure, byte[]> result = 
+      Decode.apply(
+        Encode.apply("test", false, strs),
+        bytes);
+    // print result
+    if (result.isRight()) {
+      System.out.println(
+        String.format("encoded then decoded '%s'",
+               new String(result.right().get())));
+    }
+  }
+}
